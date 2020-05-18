313e983f21619cf5419021f60199765555f16fc2
DavinMiler <27428383+DaanVink@users.noreply.github.com>
2020-05-18 17:46:59 +0200

    Cleaning up old links

--- AvatarIconViewer/AvatarIconViewer.plugin.js
+++ AvatarIconViewer/AvatarIconViewer.plugin.js
@@ -3,31 +3,7 @@
  * @website https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/aivViewer
  * @source https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/aivViewer/aivViewer.plugin.js
  */
-/*@cc_on
-@if (@_jscript)
-	
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
 
-@else@*/
-
-function print(args) { console.log(args) }
 
 var AvatarIconViewer = (_ => {
     const config = {
@@ -39,13 +15,12 @@ var AvatarIconViewer = (_ => {
             }],
         version:"1.0.0",
         description:"Allows you to view and copy a user's profile picture.",
-        github:"https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/aivViewer",
-        github_raw:"https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/aivViewer/aivViewer.plugin.js"},
+        github:"https://github.com/DaanVink/BetterDiscordPlugins/blob/master/AvatarIconViewer/AvatarIconViewer.plugin.js",
+        github_raw:"https://raw.githubusercontent.com/DaanVink/BetterDiscordPlugins/master/AvatarIconViewer/AvatarIconViewer.plugin.js"},
         defaultConfig:[
             {   type:"switch",
                 id:"contextMenus",
                 name:"Context Menus",
-                note:"Toggles colorizing of typing notifications.",
                 value:true
             }
         ],
@@ -447,4 +422,3 @@ var AvatarIconViewer = (_ => {
         return plugin(Plugin, Api);
     })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
-/*@end@*/
